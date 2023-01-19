b5aada6c8d90510034cdc4a5a89b84f4391d01a0
Jesús Pérez <jesus-perez95@hotmail.com>
2023-01-19 02:08:42 -0300

    [Add] Input PC movement with New input system

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] float _speed = 6.0f;
     [SerializeField] float _gravity = -20f;
     private CharacterController _charController;
-    
+    private PlayerInput _playerInput;
+    private InputAction _moveAction;
+    private Vector2 moveDirection;
+    private Vector3 movement;
+
     void Start()
     {
         _charController = GetComponent<CharacterController>();
+        _playerInput = GetComponent<PlayerInput>();
+        _moveAction = _playerInput.actions["Movement"];
     }
 
     void Update()
@@ -20,16 +27,14 @@ public class PlayerMovementController : MonoBehaviour
 
     private void MoveCharacter()
     {
-        float deltaX = Input.GetAxis("Horizontal") * _speed;
-        float deltaZ = Input.GetAxis("Vertical") * _speed;
 
-        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, _speed);
+        moveDirection = _moveAction.ReadValue<Vector2>();
+        //float deltaX = Input.GetAxis("Horizontal") * _speed;
+        //float deltaZ = Input.GetAxis("Vertical") * _speed;
 
+        movement = new Vector3(moveDirection.x, 0, moveDirection.y);
         movement.y = _gravity;
-        movement *= Time.deltaTime;
-        movement = transform.TransformDirection(movement);
 
-        _charController.Move(movement);
+        _charController.Move(_speed * Time.deltaTime * movement);
     }
 }
